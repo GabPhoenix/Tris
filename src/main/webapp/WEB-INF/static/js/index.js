12ce9aabe1b1d51dce3d0cf57bcc3a1b7eaaef08
gabphoenix <anjomito26@gmail.com>
2022-03-22 17:38:17 -0300

    update web-files

---
+++ src/main/webapp/WEB-INF/static/js/index.js
@@ -0,0 +1,11 @@
+function loginPageRedirect(){
+    window.location.href = "../WEB-INF/login.html"
+}
+
+function signinPageRedirect(){
+    window.location.href = "../WEB-INF/signin.html"
+}
+
+function indexPageRedirect(){
+    window.location.href = "../index.html"
+}
